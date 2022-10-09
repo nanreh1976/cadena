b6a9efe50d9235e791e4b0cea1d99fadf8a18e9d
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-09 13:28:30 -0300

    uso de cadenas en python

---
+++ cadena.py
@@ -0,0 +1,14 @@
+#                  10        20        30
+#         012345678901234567890123456789012345 
+cadena = "Soy una CaDENa de texto Para pruebas"
+
+print(len(cadena))
+print (cadena[2:14:2])
+print (cadena.capitalize())
+print (cadena.title())
+print (cadena.upper())
+print (cadena.lower())
+
+print (cadena.isalpha())
+print ("hola".isalpha())
+print ("12".isdigit())
